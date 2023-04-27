15781dacc56e91cbf102674c2f4cab9db6ad9c98
Jannery Briceño <jannerymai@gmail.com>
2023-04-27 18:02:27 -0500

    Modify app-routing Module and add routes to it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PromosComponent } from './pages/promos/promos.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { DetailComponent } from './pages/detail/detail.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PromosComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'detail/:id', component: DetailComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
