8d1b78423af9de894710878333d9a4672af4c560
Jannery Briceño <jannerymai@gmail.com>
2023-04-27 17:39:27 -0500

    Add main pages(views)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PromosComponent } from './pages/promos/promos.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { DetailComponent } from './pages/detail/detail.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PromosComponent,
+    ProductsComponent,
+    DetailComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
