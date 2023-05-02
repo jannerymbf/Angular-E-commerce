e4da3e1e988be7cbbbdc9abbcbdcf3b1a834cde7
Jannery Briceño <jannerymai@gmail.com>
2023-05-02 13:45:53 -0500

    Add select bar, and categories to it

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -9,16 +9,26 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductsComponent implements OnInit {
 
   products: any[] = [];
+  categories: any[] = [];
 
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
     this.getProducts();
+    this.getCategories();
   }
 
   getProducts() {
     return this.productService.getAllProducts().subscribe((res: any) => {
       return this.products = res;
+    })
+  }
+
+  getCategories() {
+    let allCategories: any[] = [];
+    this.productService.getAllProducts().subscribe((res) => {
+      allCategories = res.map((e: any) => e.category);
+      this.categories = [...new Set(allCategories)];
     })
   }
 
